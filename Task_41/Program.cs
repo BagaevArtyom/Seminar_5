d9a3e7e9574be19b818b8e0eea5b288e4dd7e93e
Artyom <artyombagaev150@yandex.ru>
2023-02-16 02:15:44 +0300

    Task_41 done

---
+++ Task_41/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Clear();
+
+Console.WriteLine("Введите количество числел: ");
+int countNumbers = int.Parse(Console.ReadLine()!);
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size]; 
+    for (int i=0; i<array.Length; i++)
+    {
+         Console.WriteLine("Введите число: ");
+         array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int UpperNull(int[] array1)
+{
+    int count = 0;
+    int[] array = new int[array1.Length];
+    for (int i=0; i<array1.Length; i++)
+    if (array1[i]>0)
+    {
+        count++;
+    }
+    return count;
+}
+
+int[] newArray = GetArray(countNumbers);
+Console.WriteLine(String.Join(", ", newArray));
+int positivNumber = UpperNull(newArray);
+Console.WriteLine($"Количество чисел больше нуля: {positivNumber}");
+
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.Clear();
+int input()
+{
+    Console.WriteLine("Две прямые заданы уравнениями y = k1 * x + b1, y = k2 * x + b2");
+    Console.WriteLine("Введите значение b1;");
+    int b1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите значение k1;");
+    int k1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите значение b2;");
+    int b2 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите значение k2;");
+    int k2 = int.Parse(Console.ReadLine()!);
+    return ;
+}
+
+
+int crossPoint(int b1, int k1, int b2, int k2)
+{
+    int y=0;
+    int x=0;
+   if (y-y == (k1*x+b1)-(k2*x+b2))
+    {
+        y = k1*(y-b2)/k2+b1;   //  y = k1 * x + b1, y = k2 * x + b2
+        x = (y-b2)/k2;
+        Console.WriteLine($"Прямые пересекаются в точке: {x}, {y}");
+    }
+    else
+    {
+        Console.WriteLine("Прямые не пересекаются");
+    }
+    return y;
+}
+
+crossPoint(input);
