8f8af29fca6c9dd506af684c980f2dae84c368a1
Artyom <artyombagaev150@yandex.ru>
2023-02-21 16:38:36 +0300

    Task_58

---
+++ Task_58/Program.cs
@@ -0,0 +1,87 @@
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+int[,] GetArray(int y, int x)
+{
+    int[,] result = new int[y, x];
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+int[,] Array1xArray2(int[,] inArray1, int[,]inArray2)
+{
+    int row1 = inArray1.GetLength(0);
+    int col1 = inArray1.GetLength(1);
+    int row2 = inArray2.GetLength(0);
+    int col2 = inArray2.GetLength(1);
+    int[,] result=new int[row1, col2];
+    for (int i=0; i<row1; i++)
+    {
+        for (int j=0; j<col2; j++)
+        {
+            for (int h=0; h<row2; h++)
+            {
+                result[i, j] += inArray1[i, h]*inArray2[h, j];
+            }
+        }
+    }
+    return result;
+} 
+
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Введите кол-во строк M1: ");
+    int a = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите кол-во столбцов M1: ");
+    int b = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите кол-во строк M2: ");
+    int c = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите кол-во столбцов M2: ");
+    int d = int.Parse(Console.ReadLine()!);
+    int[,] array1 = GetArray(a, b);
+    PrintArray(array1);
+    Console.WriteLine("__________________________");
+    int[,] array2 = GetArray(c, d);
+    PrintArray(array2);
+    if (b != c)
+    {
+        Console.WriteLine("Невозможно перемножить матрицы. Кол-во столбцов первой не равно кол-ву строк второй.");
+        return;
+    }
+    Console.WriteLine("__________________________");
+    int[,] result = Array1xArray2(array1, array2);
+    PrintArray(result);
+}
+
+Main();
+
+
+
+
---
+++ Task_60/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1) */
+
+int[,,] GetArray(int y, int x, int w)
+{
+    int[,,] result = new int[y, x, w];
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            for (int z = 0; z < w; z++)
+            {
+                result[i, j, z] = new Random().Next(10, 99);
+            }
+            
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            for (int z=0; z<array.GetLength(2); j++)
+            {
+                Console.Write($"{array[i, j, z]}\t");
+                
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите кол-во строк: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите кол-во столбцов: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите кол-во ячеек в глубину: ");
+int c = int.Parse(Console.ReadLine()!);
+int[,,] array = GetArray(a, b, c);
+PrintArray(array);
+
