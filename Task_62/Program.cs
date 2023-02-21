d3a8342021ae33d6a4be61faa5c525fcf00509a4
Artyom <artyombagaev150@yandex.ru>
2023-02-21 19:12:24 +0300

    Task_58_60

--- Task_62/Program.cs
+++ Task_62/Program.cs
@@ -6,3 +6,22 @@
 10 09 08 07 */
 
 
+
+void PrintArray(int[,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите кол-во строк: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите кол-во столбцов: ");
+int b = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(a, b);
+PrintArray(array);
